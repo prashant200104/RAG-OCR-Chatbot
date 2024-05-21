c916586de19e6118a109bd78d12de1054361b65f
Prashant Kumar Verma <81533918+prashant200104@users.noreply.github.com>
2024-05-21 15:34:26 +0530

    Update brain.py

--- brain.py
+++ brain.py
@@ -12,7 +12,10 @@ load_dotenv()
 # Set the TESSDATA_PREFIX environment variable
 tessdata_dir = "/usr/share/tessdata"
 # os.environ["TESSDATA_PREFIX"] = tessdata_dir
-os.environ["TESSDATA_PREFIX"] = "/usr/share/tessdata"
+#os.environ["TESSDATA_PREFIX"] = "/usr/share/tessdata"
+sudo mkdir -p /usr/share/tessdata
+sudo wget -P /usr/share/tessdata https://github.com/tesseract-ocr/tessdata/raw/main/eng.traineddata
+
 
 # Debug: Print the TESSDATA_PREFIX value
 print(f"TESSDATA_PREFIX set to: {os.environ['TESSDATA_PREFIX']}")
