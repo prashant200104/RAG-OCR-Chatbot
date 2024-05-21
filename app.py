900d3212ae9f8766dcdd9faf41b4fd8df7991bb6
Prashant Kumar Verma <81533918+prashant200104@users.noreply.github.com>
2024-05-21 15:24:09 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,125 @@
+# Import necessary libraries
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+
+# Import necessary libraries
+import databutton as db
+import streamlit as st
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
+load_dotenv()
+client = OpenAI(api_key = st.secrets["OPENAI_API_KEY"] ,)
+from brain import get_index_for_pdf
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from langchain_community.vectorstores import FAISS
+
+
+
+
+# Set the title for the Streamlit app
+st.title("RAG enhanced Chatbot")
+
+# Set up the OpenAI API key from databutton secrets
+## load the GROQ And OpenAI API KEY 
+
+# Cached function to create a vectordb for the provided PDF files
+@st.cache_resource
+def create_vectordb(files, filenames):
+    # Show a spinner while creating the vectordb
+    with st.spinner("Vector database"):
+        vectordb = get_index_for_pdf(
+            [file.getvalue() for file in files], filenames, openai_api_key = st.secrets["OPENAI_API_KEY"] )
+    return vectordb
+
+
+# Upload PDF files using Streamlit's file uploader
+pdf_files = st.file_uploader("", type="pdf", accept_multiple_files=True)
+
+# If PDF files are uploaded, create the vectordb and store it in the session state
+if pdf_files:
+    pdf_file_names = [file.name for file in pdf_files]
+    st.session_state["vectordb"] = create_vectordb(pdf_files, pdf_file_names)
+
+# Define the template for the chatbot prompt
+prompt_template = """
+    You are a helpful Assistant who answers to users questions based on multiple contexts given to you.
+
+    Keep answer correct and to the point. Try to answer from context first.
+
+    If the answer is not in the context, answer should be 'this is most likely not given in the context, modify the question may be?'
+    
+    The evidence are the context of the pdf extract with metadata. 
+    
+    Only give response and do not mention source or page or filename. If user asks for it, then tell.
+        
+    The PDF content is:
+    {pdf_extract}
+"""
+
+# Get the current prompt from the session state or set a default value
+prompt = st.session_state.get("prompt", [{"role": "system", "content": "none"}])
+
+# Display previous chat messages
+for message in prompt:
+    if message["role"] != "system":
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+
+# Get the user's question using Streamlit's chat input
+question = st.chat_input("Ask anything")
+
+# Handle the user's question
+if question:
+    vectordb = st.session_state.get("vectordb", None)
+    if not vectordb:
+        with st.message("assistant"):
+            st.write("You need to provide a PDF")
+            st.stop()
+
+    # Search the vectordb for similar content to the user's question
+    search_results = vectordb.similarity_search(question, k=5)
+    # search_results
+    pdf_extract = "/n ".join([result.page_content for result in search_results])
+
+    # Update the prompt with the pdf extract
+    prompt[0] = {
+        "role": "system",
+        "content": prompt_template.format(pdf_extract=pdf_extract),
+    }
+
+    # Add the user's question to the prompt and display it
+    prompt.append({"role": "user", "content": question})
+    with st.chat_message("user"):
+        st.write(question)
+
+    # Display an empty assistant message while waiting for the response
+    with st.chat_message("assistant"):
+        botmsg = st.empty()
+
+    # Call ChatGPT with streaming and display the response as it comes
+    response = []
+    result = ""
+    for chunk in client.chat.completions.create(
+        model= "gpt-3.5-turbo", messages=prompt, stream=True, temperature= 0.6):
+        text =  chunk.choices[0].delta.content
+
+        if text is not None:
+            response.append(text)
+            result = "".join(response).strip()
+            botmsg.write(result)
+
+    # Add the assistant's response to the prompt
+    prompt.append({"role": "assistant", "content": result})
+
+    # Store the updated prompt in the session state
+    #st.session_state["prompt"] = prompt
+    #prompt.append({"role": "assistant", "content": result})
+
+    # Store the updated prompt in the session state
+    st.session_state["prompt"] = prompt
