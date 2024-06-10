e58226e0cbc29dac35b7ceb9363eeb17335d1fbb
Prashant Kumar Verma <81533918+prashant200104@users.noreply.github.com>
2024-06-10 18:10:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,42 @@
 import openai
-from lib import *
+import re
+import os
+import io
+import tempfile
+from typing import Tuple, List
+
+from PIL import Image
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
+from reportlab.lib.utils import ImageReader
+
+import PyPDF2
+
+from dotenv import load_dotenv
+from PIL import Image
+from pdf2image import convert_from_path
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
+from reportlab.lib.utils import ImageReader
+import pytesseract
+import streamlit as st
+import openai
+
+from langchain.chains import RetrievalQA
+from langchain_community.chat_models import ChatOpenAI
+from langchain_community.vectorstores import FAISS
+
+
 from brain import get_index_for_pdf
-from document_handler import initialize_session_state, handle_file_uploads
+
+
+# Load environment variables
+load_dotenv()
+
+# Initialize OpenAI client
+openai_api_key = st.secrets["OPENAI_API_KEY"]
+
+from brain import get_index_for_pdf
 
 # Set the title for the Streamlit app
 st.title("RAG-OCR Enhanced Chatbot")
@@ -22,6 +57,93 @@ prompt_template = """
     The PDF content is:
     {pdf_extract}
 """
+
+def save_images_to_pdf(images):
+    pdf_buffer = io.BytesIO()
+    c = canvas.Canvas(pdf_buffer, pagesize=letter)
+    pdf_name = '_'.join(image_file.name for image_file in images) + '.pdf'
+
+    for image in images:
+        img = Image.open(image)
+        img_width, img_height = img.size
+        scale = min(letter[0] / img_width, letter[1] / img_height)
+        img_width *= scale
+        img_height *= scale
+        img_buffer = io.BytesIO()
+        img.save(img_buffer, format='PNG')
+        img_buffer.seek(0)
+        image_reader = ImageReader(img_buffer)
+        c.drawImage(image_reader, 0, letter[1] - img_height, width=img_width, height=img_height)
+        c.showPage()
+
+    c.save()
+    pdf_buffer.seek(0)
+    return pdf_name, pdf_buffer
+
+def initialize_session_state():
+    if 'pdf_files' not in st.session_state:
+        st.session_state.pdf_files = []
+
+    if 'image_files' not in st.session_state:
+        st.session_state.image_files = []
+
+    if 'text_pdf_files' not in st.session_state:
+        st.session_state.text_pdf_files = []
+
+    if 'show_pdfs' not in st.session_state:
+        st.session_state.show_pdfs = False
+
+def handle_file_uploads():
+    text_pdf_files = st.file_uploader("Upload Text PDF(s)", type="pdf", accept_multiple_files=True, key="text_pdf_upload")
+    uploaded_pdf_files = st.file_uploader("Scanned/Handwritten PDF(s)", type="pdf", accept_multiple_files=True)
+    image_files = st.file_uploader("Upload Image(s)", type=["png", "jpg", "jpeg", "gif"], accept_multiple_files=True, key="image_upload")
+
+    if uploaded_pdf_files is not None:
+        for file in uploaded_pdf_files:
+            st.session_state.pdf_files.append((file.name, file))
+
+    if image_files is not None:
+        st.session_state.image_files.extend(image_files)
+        if st.session_state.image_files:
+            pdf_name, pdf_buffer = save_images_to_pdf(st.session_state.image_files)
+            st.session_state.pdf_files.append((pdf_name, pdf_buffer))
+            st.session_state.image_files = []
+            st.session_state.show_pdfs = True
+
+    if text_pdf_files is not None:
+        for file in text_pdf_files:
+            #text_content = extract_text_from_text_pdf(file)
+            st.session_state.text_pdf_files.append((file.name, file))
+
+    if 'vectordbs' not in st.session_state and (st.session_state.pdf_files or st.session_state.text_pdf_files):
+        pdf_file_names = [name for name, _ in st.session_state.pdf_files]
+        pdf_buffers = [buffer for _, buffer in st.session_state.pdf_files]
+        text_pdf_file_names = [name for name, _ in st.session_state.text_pdf_files]
+        text_pdf = [text for _, text in st.session_state.text_pdf_files]
+        
+        # Store document names for later use
+        st.session_state.document_names = pdf_file_names + text_pdf_file_names
+        
+        
+        st.session_state["vectordbs"] = create_vectordb(pdf_buffers, pdf_file_names, text_pdf, text_pdf_file_names)
+
+@st.cache_resource
+def create_vectordb(image_pdf_files, image_pdf_filenames, text_pdf, text_pdf_file_names):
+    from brain import get_index_for_pdf
+
+    # Process image PDFs
+    image_vectordbs = get_index_for_pdf(
+        [file.getvalue() for file in image_pdf_files], image_pdf_filenames, openai_api_key = st.secrets["OPENAI_API_KEY"] )
+
+    # Process text PDFs
+    from brain_text import get_index_for_text_pdf
+    text_vectordbs = get_index_for_text_pdf(
+        [file.getvalue() for file in text_pdf], text_pdf_file_names, openai_api_key = st.secrets["OPENAI_API_KEY"] )
+    
+
+    return image_vectordbs + text_vectordbs
+    
+
 
 def initialize_prompt():
     if 'prompt' not in st.session_state:
