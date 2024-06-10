35167541af9ad0d899faa001a91cd92249f4c626
Prashant Kumar Verma <81533918+prashant200104@users.noreply.github.com>
2024-06-10 17:38:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,37 @@
 import openai
-from lib import *
+import re
+import os
+import io
+import tempfile
+from typing import Tuple, List
+
+import PyPDF2
+
+from dotenv import load_dotenv
+from PIL import Image
+from pdf2image import convert_from_path
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
+from reportlab.lib.utils import ImageReader
+import pytesseract
+import streamlit as st
+import openai
+
+from langchain.chains import RetrievalQA
+from langchain_community.chat_models import ChatOpenAI
+from langchain_community.vectorstores import FAISS
+
+
+from brain import get_index_for_pdf
+from document_handler import initialize_session_state, handle_file_uploads
+
+# Load environment variables
+load_dotenv()
+
+# Initialize OpenAI client
+openai_api_key = st.secrets["OPENAI_API_KEY"]
+
+
 from brain import get_index_for_pdf
 from document_handler import initialize_session_state, handle_file_uploads
 
